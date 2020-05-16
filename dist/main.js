21ba88983347062c9065bb91ffe066a6aabac8d0
Jhon Guerrero <jhoguer@gmail.com>
2020-05-15 23:08:05 -0500

    Tipando con number, boolean y string

--- dist/main.js
+++ dist/main.js
@@ -12,3 +12,22 @@ var phoneNumber = 54938484;
 var hex = 0xf00d;
 var binary = 21;
 var octal = 484;
+// Tipo: Boolean
+// Tipado Explicito
+var isPro;
+isPro = true;
+// isPro = 1 // Error
+// Inferido
+var isUserPro = false;
+isUserPro = true;
+// isUserPro = 10;  // Error
+// Tipo: String
+// Tipado Explicito
+var username = 'jhoguer';
+username = "Jhon";
+// username = true // Error
+// Template String
+// Uso de back-tick ` 
+var userInfo;
+userInfo = "\n    User Info:\n    username: " + username + "\n    firstName: " + username + " Guerrero\n    phone: " + phone + "\n    isPro: " + isPro + "\n ";
+console.log('userInfo', userInfo);
--- src/main.ts
+++ src/main.ts
@@ -14,3 +14,34 @@ let phoneNumber = 54938484;
 let hex: number = 0xf00d;
 let binary: number = 0b10101;
 let octal: number = 0o744;
+
+// Tipo: Boolean
+// Tipado Explicito
+let isPro: boolean;
+isPro = true;
+ // isPro = 1 // Error
+ 
+ // Inferido
+ let isUserPro = false;
+ isUserPro = true;
+ // isUserPro = 10;  // Error
+
+ // Tipo: String
+ // Tipado Explicito
+ let username: string = 'jhoguer';
+ username = "Jhon";
+ // username = true // Error
+
+ // Template String
+ // Uso de back-tick ` 
+ let userInfo: string;
+ userInfo = `
+    User Info:
+    username: ${username}
+    firstName: ${username} Guerrero
+    phone: ${phone}
+    isPro: ${isPro}
+ `;
+
+ console.log('userInfo', userInfo);
+
