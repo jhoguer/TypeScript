acceb91f451dbcc19e8adc9a23fd0b5e2d272fb6
Jhon Guerrero <jhoguer@gmail.com>
2020-05-25 15:11:38 -0500

    Clases

---
+++ dist/clases.js
@@ -0,0 +1,40 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var PhotoOrientation;
+(function (PhotoOrientation) {
+    PhotoOrientation[PhotoOrientation["Landscape"] = 0] = "Landscape";
+    PhotoOrientation[PhotoOrientation["Portrait"] = 1] = "Portrait";
+    PhotoOrientation[PhotoOrientation["Square"] = 2] = "Square";
+    PhotoOrientation[PhotoOrientation["Panorama"] = 3] = "Panorama";
+})(PhotoOrientation || (PhotoOrientation = {}));
+class Picture {
+    constructor(id, title, orientation) {
+        this.id = id;
+        this.title = title;
+        this.orientation = orientation;
+    }
+    // Comportamiento
+    toString() {
+        return `[id: ${this.id},
+                 title: ${this.title},
+                 orientation: ${this.orientation}
+                ]`;
+    }
+}
+class Album {
+    constructor(id, title) {
+        this.id = id;
+        this.title = title;
+        this.pictures = [];
+    }
+    addPicture(picture) {
+        this.pictures.push(picture);
+    }
+}
+const album = new Album(1, 'Personal Pictures');
+const picture = new Picture(1, 'Platzi sesion', PhotoOrientation.Square);
+const picture1 = new Picture(2, 'Jhoguer', PhotoOrientation.Landscape);
+album.addPicture(picture);
+album.addPicture(picture1);
+// console.log('Picture=>', picture.toString());
+console.log('album ', album);
---
+++ src/clases.ts
@@ -0,0 +1,55 @@
+export {};
+
+enum PhotoOrientation {
+    Landscape,
+    Portrait,
+    Square,
+    Panorama
+}
+
+class Picture {
+    // Propiedades
+    id: number;
+    title: string;
+    orientation: PhotoOrientation;
+
+    constructor(id: number, title: string, orientation: PhotoOrientation) {
+        this.id = id;
+        this.title = title;
+        this.orientation = orientation;
+    }
+
+    // Comportamiento
+    toString() {
+        return `[id: ${this.id},
+                 title: ${this.title},
+                 orientation: ${this.orientation}
+                ]`;
+    }
+}
+
+class Album {
+    id: number;
+    title: string;
+    pictures: Picture[];
+
+    constructor(id: number, title: string) {
+        this.id = id;
+        this.title = title;
+        this.pictures = [];
+    }
+
+    addPicture(picture: Picture) {
+        this.pictures.push(picture);
+    }
+}
+
+const album: Album = new Album(1, 'Personal Pictures');
+const picture: Picture = new Picture(1, 'Platzi sesion', PhotoOrientation.Square);
+const picture1: Picture = new Picture(2, 'Jhoguer', PhotoOrientation.Landscape);
+album.addPicture(picture);
+album.addPicture(picture1);
+
+// console.log('Picture=>', picture.toString());
+
+console.log('album ', album);
